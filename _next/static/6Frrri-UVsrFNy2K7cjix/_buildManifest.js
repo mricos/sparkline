2bc701c31c606c61e9678f3941866eda2741ae45
mricos <mike.ricos@gmail.com>
2022-08-23 07:54:32 +0000

    Deploying to gh-pages from @ mricos/sparkline@fe7873339efb3f3d6806c3a5a25314371176bf0c 🚀

---
+++ _next/static/6Frrri-UVsrFNy2K7cjix/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/309-bda00a42a3fc731ec5e0.js","static/css/314e21fbde76ec48b1c8.css","static/chunks/pages/index-2b31b2edf419050a67d5.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
